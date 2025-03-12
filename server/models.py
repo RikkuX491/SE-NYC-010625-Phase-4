a104bee4d531dbaf5e865daa0a8547d05856f657
Ricardo Guerra <RikkuG491@gmail.com>
2025-03-12 16:29:18 -0400

    Added solution code for Deliverables

--- server/models.py
+++ server/models.py
@@ -18,9 +18,30 @@ class Hotel(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # Deliverable # 3 solution code
+    reviews = db.relationship('Review', back_populates='hotel', cascade='all')
+    customers = association_proxy('reviews', 'customer', creator=lambda c: Review(customer=c))
+
 class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
+
+    # Deliverable # 4 solution code
+    reviews = db.relationship('Review', back_populates='customer', cascade='all')
+    hotels = association_proxy('reviews', 'hotel', creator=lambda h: Review(hotel=h))
+
+# Deliverable # 1 solution code
+class Review(db.Model, SerializerMixin):
+    
+    # Deliverable # 2 solution code
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer)
+    text = db.Column(db.String)
+    hotel_id = db.Column(db.Integer, db.ForeignKey('hotels.id'))
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    hotel = db.relationship('Hotel', back_populates='reviews')
+    customer = db.relationship('Customer', back_populates='reviews')
